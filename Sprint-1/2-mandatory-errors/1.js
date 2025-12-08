d557eff7873469e6a34aa287046d55cc56d01956
Zhenishai <113972797+Zhenishai@users.noreply.github.com>
2025-12-08 12:33:36 +0100

    solve:errors

--- Sprint-1/2-mandatory-errors/1.js
+++ Sprint-1/2-mandatory-errors/1.js
@@ -1,4 +1,7 @@
 // trying to create an age variable and then reassign the value by 1
 
-const age = 33;
+const age = 33; //const declares a constant, which cannot be reassigned after its initial value.
 age = age + 1;
+
+// let age = 33;
+
--- Sprint-1/2-mandatory-errors/4.js
+++ Sprint-1/2-mandatory-errors/4.js
@@ -1,2 +1,2 @@
-const 12HourClockTime = "20:53";
-const 24hourClockTime = "08:53";
+const 12HourClockTime = "20:53";//Variable names cannot start with a number in JavaScript.
+const 24hourClockTime = "08:53";//Variable names cannot start with a number in JavaScript.
