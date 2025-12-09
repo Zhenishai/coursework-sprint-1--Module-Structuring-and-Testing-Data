ae57f45b8d77149fd291a98db5328183d61f153e
Zhenishai Zanybek Kyzy <Zheni>
2025-12-09 12:46:09 +0100

    1Key

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -1,15 +1,11 @@
 function getAngleType(angle) {
   if (angle === 90) return "Right angle";
-  // replace with your completed function from key-implement
-
+  if (angle < 90) return "Acute angle";
+  if (angle > 90 && angle < 180) return "Obtuse angle";
+  if (angle === 180) return "Straight angle";
+  if (angle > 180 && angle < 360) return "Reflex angle";
+  return "Invalid angle"; 
 }
-
-
-
-
-
-
-
 
 // Don't get bogged down in this detail
 // Jest uses CommonJS module syntax by default as it's quite old
