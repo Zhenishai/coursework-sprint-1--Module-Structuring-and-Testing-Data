d16424a24a25fbe5ec3dfcaf6983342c09197a7e
Zhenishai Zanybek Kyzy <Zheni>
2025-12-09 10:48:09 +0100

    debug:2MandatoryDebug/1

--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,5 +1,5 @@
 // Predict and explain first...
-//  =============> write your prediction here
+//  =============> The output will be:The sum of 10 and 32 is undefined
 
 function sum(a, b) {
   return;
@@ -8,6 +8,6 @@ function sum(a, b) {
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
 
-// =============> write your explanation here
+// =============> the function returns before it reaches a + b.The return; line exits the function immediately with no value.
 // Finally, correct the code to fix the problem
-//  =============> write your new code here
+//  =============> function sum(a, b) {return a + b;}console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
