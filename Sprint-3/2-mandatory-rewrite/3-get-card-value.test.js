a69d32333ab843573e63623fbaf771d51faae88f
Zhenishai Zanybek Kyzy <Zheni>
2025-12-09 13:24:20 +0100

    2MandatoryRewrite:3.3

--- Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
+++ Sprint-3/2-mandatory-rewrite/3-get-card-value.test.js
@@ -1,11 +1,28 @@
 const getCardValue = require("./3-get-card-value");
 
+//Case1:Ace
 test("should return 11 for Ace of Spades", () => {
-    const aceofSpades = getCardValue("A♠");
-    expect(aceofSpades).toEqual(11);
-    });
-
-// Case 2: Handle Number Cards (2-10):
-// Case 3: Handle Face Cards (J, Q, K):
-// Case 4: Handle Ace (A):
-// Case 5: Handle Invalid Cards:
+  expect(getCardValue("A♠")).toEqual(11);
+});
+//Case2:Number Cards (2-10)
+test("should return correct value for number cards", () => {
+  expect(getCardValue("5♥")).toEqual(5);
+  expect(getCardValue("2♦")).toEqual(2);
+  expect(getCardValue("10♣")).toEqual(10);
+});
+//Case3:Face Cards (J, Q, K)
+test("should return 10 for face cards J, Q, K", () => {
+  expect(getCardValue("J♣")).toEqual(10);
+  expect(getCardValue("Q♦")).toEqual(10);
+  expect(getCardValue("K♥")).toEqual(10);
+});
+//Case4:Ace again(to check consistency)
+test("should return 11 for Ace of any suit", () => {
+  expect(getCardValue("A♦")).toEqual(11);
+  expect(getCardValue("A♥")).toEqual(11);
+});
+//Case5:Invalid Cards
+test("should throw error for invalid card rank", () => {
+  expect(() => getCardValue("1♠")).toThrow("Invalid card rank.");
+  expect(() => getCardValue("B♣")).toThrow("Invalid card rank.");
+});
