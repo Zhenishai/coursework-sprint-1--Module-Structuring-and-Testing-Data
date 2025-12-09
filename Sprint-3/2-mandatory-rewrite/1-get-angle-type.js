b68e3ab7f49a9db39f211c51f553febfef7e9b5b
Zhenishai Zanybek Kyzy <Zheni>
2025-12-09 12:47:47 +0100

    2MandatoryRewrite:1

--- Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
+++ Sprint-3/2-mandatory-rewrite/1-get-angle-type.js
@@ -6,7 +6,6 @@ function getAngleType(angle) {
   if (angle > 180 && angle < 360) return "Reflex angle";
   return "Invalid angle"; 
 }
-
 // Don't get bogged down in this detail
 // Jest uses CommonJS module syntax by default as it's quite old
 // We will upgrade our approach to ES6 modules in the next course module, so for now 
