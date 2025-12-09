275e15e5bb81947f8f59aa19ae14507598125ece
Zhenishai Zanybek Kyzy <Zheni>
2025-12-09 10:18:34 +0100

    solve:1KeyError/1

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -1,20 +1,27 @@
 // Predict and explain first...
 
 // Why will an error occur when this program runs?
-// =============> write your prediction here
+// =============> JavaScript throws:ReferenceError: decimalNumber is not defined
 
 // Try playing computer with the example to work out what is going on
 
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
+  const decimalNumber = 0.5; //This re-declares the same variable name, which is not allowed with const.
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
 
-console.log(decimalNumber);
+console.log(decimalNumber);// decimalNumber has never been defined globally, only inside the function.
 
-// =============> write your explanation here
+// =============>decimalNumber is used outside its scope.the variable decimalNumber is declared twice — once as a parameter and again as a const, which is also an error.
 
 // Finally, correct the code to fix the problem
 // =============> write your new code here
+
+function convertToPercentage(decimalNumber) {
+  const percentage = `${decimalNumber * 100}%`;
+  return percentage;
+}
+
+console.log(convertToPercentage(0.5));
